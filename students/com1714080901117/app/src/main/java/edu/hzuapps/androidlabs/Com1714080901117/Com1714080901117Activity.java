2cb09519c2fb8830ea35184503252d3d1ec733fb
1714080901117 <2024265834@qq.com>
2019-03-20 19:31:34 +0800

    #2 #1714080901117 第二次实验

---
+++ students/com1714080901117/app/src/main/java/edu/hzuapps/androidlabs/Com1714080901117/Com1714080901117Activity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class Com1714080901117Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1714080901117);
+    }
+}
